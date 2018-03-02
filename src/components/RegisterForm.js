34d86cf9a3cbeb02ac0145b0a6cfed287b5ca193
henry <siauhenry@gmail.com>
2018-03-02 00:37:06 -0800

    feat: add validators for RegisterForm

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import { orange500 } from 'material-ui/styles/colors';
 
 const styles = {
-    loginButton: {
-        margin: '10px 10px 5px 90px'
+    psdCnfErrStyle: {
+        color: orange500,
     }
 };
 
@@ -17,42 +18,100 @@ export default class RegisterForm extends React.Component {
             email: '',
             phoneNumber: '',
             password: '',
-            confirmPassword: ''
+            confirmPassword: '',
+            userNameErrMessage: '',
+            passwordErrMessage: '',
+            passwordCnfErrMessage: '',
+            emailErrMessage: '',
+            phoneNumberErrmessage: '',
+            isEmailFormatIncorrect: false
+
         };
+    }
+
+    ValidateEmailFormat(email) {
+        if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+            this.setState({
+                emailErrMessage: '',
+                isEmailFormatIncorrect: false
+            });
+        } else {
+            this.setState({
+                emailErrMessage: 'Email formate incorrect',
+                isEmailFormatIncorrect: true
+            });
+        }
+    };
+    checkEmailFormat = (event)=>{
+        this.ValidateEmailFormat(this.state.email);
+    }
+    ValidateEmailAvailable(email) {
+        //TODO check with server if Email is already been used.
     }
 
     handleUserNameChange = (event) => {
         this.setState({
-            userName: event.target.value
+            userName: event.target.value,
+            userNameErrMessage: ''
         });
+        if (event.target.value.length > 20) {
+            this.setState({
+                userNameErrMessage: 'User Name must be shorter than 20 charactors'
+            });
+        }
     };
 
     handleEmailChange = (event) => {
         this.setState({
             email: event.target.value
         });
+        if (this.state.isEmailFormatIncorrect) {
+            this.ValidateEmailFormat(event.target.value);
+        }
     };
 
     handlePhoneNumberChange = (event) => {
+        // validate phone number could be a big project
+        // because different county have different phone number patten
+        // here we skip this part
         this.setState({
-            phoneNumber: event.target.value
+            phoneNumber: event.target.value,
+
         });
     };
 
     handlePasswordChange = (event) => {
         this.setState({
-            password: event.target.value
+            password: event.target.value,
+            passwordErrMessage: "Password must..."
         });
     };
+    showPasswordRequirements = () => {
+        this.setState({
+            passwordErrMessage: "Password must..."
+        });
+    }
 
     handleCnfPasswordChange = (event) => {
-        this.setState({
-        confirmPassword: event.target.value
-        });
+
+        if (event.target.value != this.state.password) {
+            this.setState({
+                passwordCnfErrMessage: `password doesn't match`
+            });
+        } else {
+            this.setState({
+                confirmPassword: event.target.value,
+                passwordCnfErrMessage: ''
+            });
+        }
     };
 
+
+
+
     handleRegister = () => {
-        alert(this.state.email + this.state.password);
+        this.ValidateEmailFormat(this.state.email)
+
     }
 
     render() {
@@ -61,28 +120,37 @@ export default class RegisterForm extends React.Component {
                 <TextField
                     hintText="User Name"
                     floatingLabelText="User Name"
-                    onChange={this.handleUserNameChange}
+                    errorText={this.state.userNameErrMessage}
+                    onFocus={this.checkEmailFormat}
                 /><br />
                 <TextField
                     hintText="Email"
                     floatingLabelText="Email"
+                    errorText={this.state.emailErrMessage}
                     onChange={this.handleEmailChange}
                 /><br />
                 <TextField
                     hintText="Phone Number"
                     floatingLabelText="Phone Number"
+                    errorText={this.state.phoneNumberErrmessage}
                     onChange={this.handlePhoneNumberChange}
+                    onChange={this.handleUserNameChange}
+
                 /><br />
                 <TextField
                     hintText="Password"
                     floatingLabelText="Password"
                     type="password"
+                    errorText={this.state.passwordErrMessage}
                     onChange={this.handlePasswordChange}
+                    onFocus={this.showPasswordRequirements}
                 /><br />
                 <TextField
                     hintText="Confirm Password"
                     floatingLabelText="Confirm Password"
                     type="password"
+                    errorText={this.state.passwordCnfErrMessage}
+                    errorStyle={styles.psdCnfErrStyle}
                     onChange={this.handleCnfPasswordChange}
                 /><br />
                 <RaisedButton
