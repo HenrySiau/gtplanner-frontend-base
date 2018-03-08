23ff74d516ece1dd1abde858cda945c4489b8890
henry <siauhenry@gmail.com>
2018-03-07 22:43:13 -0800

    feat: add local authorization

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,32 @@ import GTPAppBar from './GTPAppBar';
 import GTPDrawer from './GTPDrawer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MainSection from '../sections/MainSection';
-import LoginSection from '../sections/LoginSection'
-import RegisterSection from '../sections/RegisterSection'
+import LoginSection from '../sections/LoginSection';
+import RegisterSection from '../sections/RegisterSection';
+import DashBoardSection from '../sections/DashBoardSection';
 import {
     BrowserRouter as Router,
     Route,
     Redirect
 } from 'react-router-dom';
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem('id_token') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
 
 class App extends Component {
     constructor(props) {
@@ -43,7 +61,7 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
+            <Router >
                 <MuiThemeProvider>
                     <div className="container">
 
@@ -66,9 +84,10 @@ class App extends Component {
                             <Route exact path="/register"
                                 render={(props) => (<RegisterSection {...props} />)}
                             />
-                            <Route exact path="/"
+                            {/* <Route exact path="/"
                                 render={(props) => (<MainSection {...props} />)}
-                            />
+                            /> */}
+                            <PrivateRoute path="/dashboard" component={DashBoardSection} />
                         </div>
                     </div>
                 </MuiThemeProvider>
---
+++ src/sections/DashBoardSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashBoardSection = ()=>(
+<div>
+    <h1>To be fill DashBoard</h1>
+</div>
+);
+
+export default DashBoardSection;
