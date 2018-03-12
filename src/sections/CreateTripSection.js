873e6ad9f82f092d48d713ddf746d1b60dc481e8
henry <siauhenry@gmail.com>
2018-03-12 14:50:16 -0700

    style: add title to create trip page

--- src/sections/CreateTripSection.js
+++ src/sections/CreateTripSection.js
@@ -2,9 +2,14 @@ import React from 'react';
 import CreateTripForm from '../components/CreateTripForm';
 import '../css/CreateTripSection.css';
 const CreateTripSection = (props) => (
-    <div className="createTripSection">
-        <img src="/images/login.png" className="image" alt="login"></img>
+    <div className="createTripSectionContainer">
+        <h1 className="title">Start a new trip</h1>
+        <div className="createTripSection">
+            <div className="infoSection">
+                <img src="/images/login.png" className="image" alt="login"></img>
+            </div>
             <CreateTripForm className="createTripForm" {...props} />
+        </div>
     </div>
 
 
